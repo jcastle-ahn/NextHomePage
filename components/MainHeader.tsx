1651345d161c71ce7fb1d383810ec25e1001a72f
JCastle <jcastle.ahn@gmail.com>
2022-10-23 22:48:33 +0900

    apply initial setting

---
+++ components/MainHeader.tsx
@@ -0,0 +1,8 @@
+export default function MainHeader() {
+  return (
+    <>
+      <header>Header</header>
+      <style jsx>{``}</style>
+    </>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import { RecoilRoot } from 'recoil'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </RecoilRoot>
+  )
 }
-
-export default MyApp
